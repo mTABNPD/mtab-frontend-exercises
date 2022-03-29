7ca5faba36a21971ce9ff37217434a02abb7f014
Joe Garnett <jgarnett@mtab.com>
2022-03-29 15:45:23 +0100

    removed unused import

--- social-feed/src/App.tsx
+++ social-feed/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'hooks/store';
 import { getPosts } from 'reducers/feed/actions';
