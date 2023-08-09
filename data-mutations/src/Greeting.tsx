c3fcee8cd68fb5afd749476ee370104ea5ff16dc
Joe Garnett <jgarnett@mtab.com>
2023-08-09 14:46:11 +0100

    mutate display name

--- data-mutations/src/Greeting.tsx
+++ data-mutations/src/Greeting.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
+
+import { useGetProfileQuery, useMutateProfileMutation } from 'services/api';
+
 export function Greeting() {
+  const { data: profile, isLoading } = useGetProfileQuery();
+  const [triggerMutation] = useMutateProfileMutation();
+  const [displayName, setDisplayName] = useState<string>('');
+
+  useEffect(() => {
+    if (!profile) {
+      return
+    }
+
+    setDisplayName(profile.displayName)
+  }, [profile]);
+
+  if (isLoading || !profile) {
+    return 'Loading...';
+  }
+
   return (
-    <h1>
-      Hello,
+    <>
+      <h1>
+        Hello,
+        {' '}
+        {profile.displayName}
+      </h1>
+      <input
+        value={displayName}
+        onChange={(e) => setDisplayName(e.target.value)}
+      />
       {' '}
-      World!
-    </h1>
+      <button
+        onClick={() => displayName && triggerMutation({ ...profile, displayName })}
+        disabled={!displayName}
+      >
+        Change name
+      </button>
+    </>
   );
 }
--- data-mutations/src/services/api/api.ts
+++ data-mutations/src/services/api/api.ts
@@ -1,7 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { PROFILE_URL } from 'consts/api';
+import { UserProfile } from 'types/user';
+
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery(),
-  endpoints: () => ({ /* endpoints are injected */})
+  tagTypes: ['PROFILE'],
+  endpoints: (builder) => ({
+    getProfile: builder.query<UserProfile, void>({
+      query: () => PROFILE_URL,
+      providesTags: () => [{ type: 'PROFILE' }]
+    }),
+    mutateProfile: builder.mutation<UserProfile, UserProfile>({
+      query: (profile) => ({
+        url: PROFILE_URL,
+        method: 'PUT',
+        body: profile
+      }),
+      invalidatesTags: () => [{ type: 'PROFILE' }]
+    })
+  })
 });
+
+export const { useGetProfileQuery, useMutateProfileMutation } = api;
