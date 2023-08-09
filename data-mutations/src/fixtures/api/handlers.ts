4cad81dc2f909885920630c596be81398ee77729
Joe Garnett <jgarnett@mtab.com>
2023-08-09 14:17:59 +0100

    added mutation handler

--- data-mutations/src/fixtures/api/handlers.ts
+++ data-mutations/src/fixtures/api/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 
 import { createUserProfile } from 'fixtures/user';
 
-const profile = createUserProfile();
+let profile = createUserProfile();
 
 // default handlers
 export const handlers = {
@@ -12,10 +12,22 @@ export const handlers = {
       ctx.json(profile)
     )
   )),
-  putProfile: rest.put('*/api/user/profile', (_, res, ctx) => (
-    res(
-      ctx.status(200),
-      ctx.json(profile)
-    )
-  ))
+  putProfile: rest.put('*/api/user/profile', (
+    req,
+    res,
+    ctx) => {
+
+    return req.json()
+      .then((data) => {
+        profile = {
+          ...profile,
+          ...data
+        }
+
+        return res(
+          ctx.status(200),
+          ctx.json(profile)
+        );
+      })
+  })
 };
